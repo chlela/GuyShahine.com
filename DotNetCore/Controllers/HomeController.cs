d7fbfce49967d3de72b84fd0326bc635ff02a1ba
Guy Shahine <guy@gshahine.com>
2016-10-09 11:29:34 -0700

    remove bootstrap. Cleanup project from unwanted assets and html

--- DotNetCore/Controllers/HomeController.cs
+++ DotNetCore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DotNetCore.Controllers
 
     public IActionResult About()
     {
-      ViewData["Message"] = "Your application description page.";
+      ViewData["Message"] = "";
 
       return View();
     }
--- DotNetCore/Program.cs
+++ DotNetCore/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-
-namespace DotNetCore
+﻿namespace DotNetCore
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Hosting;
 
-            host.Run();
-        }
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseIISIntegration()
+          .UseStartup<Startup>()
+          .Build();
+
+      host.Run();
     }
+  }
 }
