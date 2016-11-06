6f8b00954c6d59a5bf5e0ce7154d3ae22c28b0fb
Guy Shahine <guy@resoltz.com>
2016-11-05 18:27:00 -0700

    update with new validation strings

--- DotNetCore/Controllers/HomeController.cs
+++ DotNetCore/Controllers/HomeController.cs
@@ -35,14 +35,14 @@
       return View();
     }
 
-    [Route(".well-known/acme-challenge/WP9UtlstQdjCq_lQrJXFtuk5OGjCiv2CE2KKc7mVDeo")]
+    [Route(".well-known/acme-challenge/bsAANM6dd6OlpX07uXMkISJX3l9aY7whGIZSD6yuW5o")]
     public IActionResult AcmeDomainValidation() {
-      return new FileContentResult(Encoding.UTF8.GetBytes("WP9UtlstQdjCq_lQrJXFtuk5OGjCiv2CE2KKc7mVDeo.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
+      return new FileContentResult(Encoding.UTF8.GetBytes("bsAANM6dd6OlpX07uXMkISJX3l9aY7whGIZSD6yuW5o.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
     }
 
-     [Route(".well-known/acme-challenge/5AggMXC22QYjsGb8lLIvbwvpsKQFgIrfQb-YzKO5IEM")]
+     [Route(".well-known/acme-challenge/oW6HgjwN7ZLzL7A9rqza7khxGY7evAGNOhiWa4xOxJ4")]
     public IActionResult AcmeDomainValidation2() {
-      return new FileContentResult(Encoding.UTF8.GetBytes("5AggMXC22QYjsGb8lLIvbwvpsKQFgIrfQb-YzKO5IEM.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
+      return new FileContentResult(Encoding.UTF8.GetBytes("oW6HgjwN7ZLzL7A9rqza7khxGY7evAGNOhiWa4xOxJ4.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
     }
   }
 }
