888bb10016a0d279b9ebc777b8e5fcd9d4ee799c
Guy Shahine <guy@resoltz.com>
2016-11-05 18:23:21 -0700

    add the validation file for www domain

--- DotNetCore/Controllers/HomeController.cs
+++ DotNetCore/Controllers/HomeController.cs
@@ -39,5 +39,10 @@
     public IActionResult AcmeDomainValidation() {
       return new FileContentResult(Encoding.UTF8.GetBytes("WP9UtlstQdjCq_lQrJXFtuk5OGjCiv2CE2KKc7mVDeo.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
     }
+
+     [Route(".well-known/acme-challenge/5AggMXC22QYjsGb8lLIvbwvpsKQFgIrfQb-YzKO5IEM")]
+    public IActionResult AcmeDomainValidation2() {
+      return new FileContentResult(Encoding.UTF8.GetBytes("5AggMXC22QYjsGb8lLIvbwvpsKQFgIrfQb-YzKO5IEM.u10OB683-R5VroKL7vZdOs75_EXzVyh9cOfccXZ4rcc"), "text/plain");
+    }
   }
 }
